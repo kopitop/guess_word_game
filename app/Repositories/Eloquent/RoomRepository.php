dbcd3f6bd2303aa4ff9ff58e28305462a1953027
Nguyen Minh Hiep <hiep24892@gmail.com>
2016-12-17 13:42:41 +0700

    User Room Submit Answer

--- app/Repositories/Eloquent/RoomRepository.php
+++ app/Repositories/Eloquent/RoomRepository.php
@@ -317,4 +317,40 @@ class RoomRepository extends BaseRepository implements RoomRepositoryInterface
 
         return $data;
     }
+    
+    /**
+     * Post an answer
+     *
+     * @param array $input
+     *
+     * @return mixed
+     */
+    public function postAnswer($input)
+    {   
+        $id = $input['id'];
+        $answer = $input['answer'];
+        $room = $this->model->findOrFail($id);
+        if ($room->status != config('room.status.playing')) {
+            throw new Exception;
+        }
+
+        $current_round = $room->results->last();
+
+        //Update image
+        $current_round->fill([
+            'answer' => $answer,
+            'is_correct' => $current_round->word->content == $answer
+        ]);
+
+        if (!$current_round->save()) {
+            throw new Exception;
+        }
+
+        $data = [
+            'room' => $room,
+            'current_round' =>  $current_round,
+        ];
+
+        return $data;
+    }
 }
